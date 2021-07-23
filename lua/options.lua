c581a58257db2252ae16405aac16a400eb0ad172
KureijiR <yuuki.kureiji@gmail.com>
2021-07-23 22:57:37 +0700

    Change options, enable persistent undo

--- lua/options.lua
+++ lua/options.lua
@@ -14,13 +14,15 @@ opt.ignorecase = true
 opt.splitbelow = true
 opt.splitright = true
 opt.termguicolors = true
-opt.cul = true
+opt.cursorline = true
 opt.mouse = "a"
 opt.signcolumn = "yes"
 opt.cmdheight = 1
-opt.updatetime = 250 -- update interval for gitsigns
+opt.updatetime = 500 -- update interval for gitsigns
+--opt.timeout = false
 opt.timeoutlen = 400
 opt.clipboard = "unnamedplus"
+opt.scrolloff = 10
 
 -- disable nvim intro
 opt.shortmess:append("sI")
@@ -36,14 +38,45 @@ opt.numberwidth = 2
 -- Indenline
 opt.expandtab = true
 opt.shiftwidth = 2
+opt.softtabstop = -1
+opt.autoindent = true
 opt.smartindent = true
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
-opt.whichwrap:append("<>hl")
+--opt.whichwrap:append("<>hl")
 
 g.mapleader = " "
 g.auto_save = false
+
+-- Keep undo history across sessions by storing it in a file
+if vim.fn.has('persistent_undo') == 1 then
+    local paths = {
+        "/tmp/nvim/undo",
+        (os.getenv("PREFIX") or "") .. "/tmp/nvim/undo",
+    }
+
+    for key,value in ipairs(paths)
+    do
+        os.execute("mkdir -p -m700 " .. value)
+        local ok, err, code = os.rename(value, value)
+        if ok then
+            opt.undodir = value
+            opt.undofile = true
+
+            break
+        end
+    end
+end
+opt.undolevels = 1000
+opt.undoreload = 10000
+
+-- use ripgreg instead of grep
+opt.grepprg="rg --vimgrep"
+
+-- crontab filetype tweak (the way vim normally saves files confuses crontab
+-- so this workaround allows for editing
+vim.cmd("autocmd FileType crontab setlocal bkc=yes")
 
 -- disable builtin vim plugins
 local disabled_built_ins = {
